d101cae5c5c01db84cb463939fcf0a4a8da16cff
shingalahitesh <shingalahitesh2@gmail.com>
2023-11-03 12:55:26 +0530

    - update husky configuration.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,13 +4,13 @@ import { withPrivateRoute } from "@/HOC/privateRoute";
 
 function Home1() {
 	return (
-<main>
-<div style={{ height: "100vh", backgroundColor: "white" }}>
-	<div>
-		<h2>Welcome to FrontEnd Template</h2>
-	</div>
-</div>
-</main>
+		<main>
+			<div style={{ height: "100vh", backgroundColor: "white" }}>
+				<div>
+					<h2>Welcome to FrontEnd Template</h2>
+				</div>
+			</div>
+		</main>
 	);
 }
 
