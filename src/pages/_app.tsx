e1922a4e23e9358d748fc824c4b6adb37ee50a3b
Dhiraj Poojary <dhiraj.poojary@vships.com>
2024-10-08 14:33:08 +0530

    fix: access denied issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from "next/app";
 import Providers from "@/redux/provider";
 import { AuthProvider } from "@/authcontext/AuthContext";
-import useApplicationAccess from "@/hooks/customhooks/useApplicationAccess";
 import RootLayout from "../global/layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	useApplicationAccess();
 	return (
 		<Providers>
 			<AuthProvider>
