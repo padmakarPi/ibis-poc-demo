253b8464e9e881b50c5c9e9972a02d1d4e3c1b52
Anisha Yadav <2000anishayadav@gmail.com>
2024-12-23 18:47:39 +0530

    Update next.config.js and Versions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    // ENABLE BASE PATH: Uncomment base path config.
+  // ENABLE BASE PATH: Uncomment base path config.
     // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    experimental: {
+        optimizePackageImports: ["@vplatform/shared-components"]
+      },
 }
 
 module.exports = nextConfig
