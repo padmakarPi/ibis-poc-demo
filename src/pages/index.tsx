aa56ca3d77b9d18a23669906c9a50563f703e3ee
Anisha Yadav <2000anishayadav@gmail.com>
2025-01-03 12:28:49 +0530

    change layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,7 @@
 "use client";
 
-import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
-import { AuthContext } from "@/authcontext/AuthContext";
 import WelcomeScreenMicroFrontEnd from "@/components/microfrontends/WelcomeScreenMicrofrontend";
-import { SESSION_STORAGE_KEYS } from "@/lib/constant/oidc";
-import { getOriginalRoute } from "@/lib/utils";
 
 export default function Main() {
-	const { getUser, login } = useContext(AuthContext);
-	const router = useRouter();
-	const initializeAuth = async () => {
-		try {
-			const user = await getUser();
-			if (user) {
-				const originalRoute = getOriginalRoute();
-				router.push(originalRoute);
-			} else {
-				await login();
-			}
-			return null;
-		} catch (error) {
-			console.log("error=", error);
-			return null;
-		}
-	};
-
-	useEffect(() => {
-		const isNotCallbackRoute = !router.asPath.startsWith("/auth/oidc-callback");
-		if (isNotCallbackRoute) {
-			sessionStorage.setItem(SESSION_STORAGE_KEYS.ORIGINALROUTE, router.asPath);
-		}
-		if (isNotCallbackRoute) {
-			initializeAuth();
-		}
-	}, []);
-
 	return <WelcomeScreenMicroFrontEnd currentStep={0} IsCallBackPage={false} />;
 }
